457f6f8790bf633ebce0a77571a9ee93e85a31a6
moon <imtommy7@gmail.com>
2025-10-15 01:24:30 +0400

    Init task 1

---
+++ task_1.py
@@ -0,0 +1,22 @@
+# Лабораторная №1: Первичная инициализация
+# Курс: Основы теории систем
+# Студент: [Ваше ФИО]
+
+def get_system_info():
+    """
+    Эта функция должна вернуть словарь с информацией о вашей "системе".
+    """
+    # TODO: Заполните словарь вашими реальными данными
+    system_info = {
+        "student_name": "Чагаев Айдар Наргизович",
+        "academic_group": "ВТАСбз-21",
+        "github_link": "https://github.com/m00nlez"
+    }
+    return system_info
+
+# Вывод информации для проверки
+if __name__ == "__main__":
+    info = get_system_info()
+    print("Информация о системе:")
+    for key, value in info.items():
+        print(f"- {key}: {value}")
